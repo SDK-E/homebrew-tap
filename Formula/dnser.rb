eac0ce19f09d42fc0a71457129d57d80adc55b5e
Hicham SADDEK <hicham@sdk.enterprises>
2026-08-23 09:29:18 +0200

    dnser 1.0.1

--- Formula/dnser.rb
+++ Formula/dnser.rb
@@ -1,7 +1,7 @@
 class Dnser < Formula
   desc "Local DNS management for development"
   homepage "https://github.com/SDK-E/dnser"
-  version "1.0.0"
+  version "1.0.1"
   license "BUSL-1.1"
 
   livecheck do
@@ -12,20 +12,20 @@ class Dnser < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/SDK-E/dnser/releases/download/v#{version}/dnser_#{version}_Darwin_arm64.tar.gz"
-      sha256 "8992e3169feaecf4ce676687fdd79d38c09f7f39613f5eca768cf6eef612f8b6"
+      sha256 "aac317c3958ec5efb23019950761dce02490039a8ad246cd82347d59d5d1d056"
     else
       url "https://github.com/SDK-E/dnser/releases/download/v#{version}/dnser_#{version}_Darwin_amd64.tar.gz"
-      sha256 "687ebe99a2158787d10977d071755f7c8a330b07083589710e790d3b1b358f4b"
+      sha256 "6446d756cb80cff0005f48265ac0922275a836468b967a98acb719fcfdacdd40"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
       url "https://github.com/SDK-E/dnser/releases/download/v#{version}/dnser_#{version}_Linux_arm64.tar.gz"
-      sha256 "6faa992858c4cf9d02e50964d0aaa1bbd5b9f50bce482d77f221c37e18b2aade"
+      sha256 "b72d4bf6198b27b025aa1c5483a64522d20ef66f1e941cbb0636083f40427572"
     else
       url "https://github.com/SDK-E/dnser/releases/download/v#{version}/dnser_#{version}_Linux_amd64.tar.gz"
-      sha256 "88ba409530a39aadb872ebb8b852329d2cf9594bb3ab5d55aa954286236cc175"
+      sha256 "9f41e2f8c19b64d009c5fee9958b1dc67d50ba51012179edbe31fe3e5b212ddd"
     end
   end
 
@@ -38,6 +38,8 @@ class Dnser < Formula
       Run the one-time guided setup:
         dnser setup
 
+      Setup starts dnser as a privileged daemon so port 53 works, verifies it,
+      and only then points your system DNS at it.
       Dashboard: dnser open
     EOS
   end
